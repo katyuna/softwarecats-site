a1dd6ff5436aeaf8dfc2da1e97e1b31713f0a6fc
Ekaterina Zarubina <edubovenko@gmail.com>
2022-04-08 18:20:02 +0700

    PO for ContactPage

--- src/main/java/dev/po/ContactsPage.java
+++ src/main/java/dev/po/ContactsPage.java
@@ -5,7 +5,46 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class ContactsPage {
+    public static final String URL_CONTACTS = "https://softwarecats.dev/#rec394189455";
+
     //Поле "Как вас зовут?"
     @FindBy(how = How.XPATH, using = ".//input[@name='name']")
     public SelenideElement inputName;
+    //Поле "Укажите любой удобный контакт"
+    @FindBy(how = How.XPATH, using = ".//input[@name='Input']")
+    public SelenideElement inputContact;
+    //Поле "Что вы хотите обсудить"
+    @FindBy(how = How.XPATH, using = ".//input[@name='Input']")
+    public SelenideElement inputAbout;
+    //Кнопка "Отправить заявку"
+    @FindBy(how = How.XPATH, using = ".//button[@type='submit']")
+    public SelenideElement buttonSubmit;
+
+
+    //Методы заполнения полей формы и клик по кнопке
+    public void setName(String name)
+    {
+        this.inputName.click();
+        this.inputName.setValue(name);
+    }
+    public void setContact(String contact)
+    {
+        this.inputContact.click();
+        this.inputContact.setValue(contact);
+    }
+    public void setAbout(String about)
+    {
+        this.inputContact.click();
+        this.inputContact.setValue(about);
+    }
+
+    public void clickSubmitButton(){
+        this.buttonSubmit.click();
+    }
+    public  void fillTheForm (String name, String contact, String about){
+        setName(name);
+        setContact(contact);
+        setAbout(about);
+        clickSubmitButton();
+    }
 }
--- src/test/java/dev/contacts/SendContacts.java
+++ src/test/java/dev/contacts/SendContacts.java
@@ -1,6 +1,12 @@
 package dev.contacts;
 
 import dev.BaseTest;
+import dev.po.ContactsPage;
+import dev.po.MainPage;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class SendContacts extends BaseTest {
+    //Открыть страницу контактов
+    ContactsPage contactsPage = open(ContactsPage.URL_CONTACTS, ContactsPage.class);
 }
